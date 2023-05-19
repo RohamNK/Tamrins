b9b1faa51a713d6f81e4a09a4c1baeee2f69176a
RohamNK <128716736+RohamNK@users.noreply.github.com>
2023-05-19 19:01:54 +0330

    Delete Tamrins3 directory

--- Tamrins3/t04 Maar.py
+++
@@ -1,3 +0,0 @@
-n = int(input("Maaret chan metre? "))
-Maar = "*#" * (n // 2) + ("*" if n % 2 == 1 else "")
-print(Maar,"8-----<")
--- Tamrins3/t06 kmmmm.py
+++
@@ -1,7 +0,0 @@
-num1 = int(input("Adade aval: "))
-num2 = int(input("Adade dovom: "))
-greater = max(num1, num2)
-for kmm in range(greater, num1 * num2 + 1, greater):
-    if kmm % num1 == 0 and kmm % num2 == 0:
-        break
-print("KMM", num1, "va", num2, "mishe", kmm)
